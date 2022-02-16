ab6f0d0b129ffb939df08da24a3a89d7813c3f16
이진실 <ljsil1204@naver.com>
2022-02-16 18:11:26 +0900

    실제 데이터 담아주기 주석 추가

--- app/src/main/java/com/neppplus/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20220216/MainActivity.kt
@@ -20,8 +20,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
-        mStudentList.add( StudentData( "조경진", 1988, "서울시 동대문구" ) )
+
+//        실제 데이터 담아주기
+       mStudentList.add( StudentData( "조경진", 1988, "서울시 동대문구" ) )
         mStudentList.add( StudentData( "고재천", 1988, "서울시 중랑구" ) )
         mStudentList.add( StudentData( "방우진", 1983, "경기도 고양시" ) )
         mStudentList.add( StudentData( "서정민", 1984, "경기도 광명시" ) )
